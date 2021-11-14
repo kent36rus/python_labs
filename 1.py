641f92b445b449b24b0854a124c8776b5fd41e39
kent36rus <94308903+kent36rus@users.noreply.github.com>
2021-11-14 19:30:24 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+print("Введите числа")
+x=input("Введите пожалуйста x")
+y=input("Введите будьте добры y")
+x=int(x) * 2
+y=int(y) - 2
+if x>y:
+    print("x > y")
+elif x<y:
+    print("x < y")    
+else:
+    print("x = y")
